b4b2cd70049423f2f7bcdf35a43a3b29ff61bc16
narmold <Nick@Nicks-MacBook-Pro.local>
2015-11-18 19:15:39 -0500

    gasdafdsf

---
+++ app/src/main/java/com/sourcey/materiallogindemo/MainActivity.java
@@ -0,0 +1,54 @@
+package com.sourcey.materiallogindemo;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "OCmVLvcobIs6QCtZ0loOHrennK77e5MkEglD7B5p", "IlSI34swUX56B9CnvSFOdKcJPIiRtcQRhBiIFRge");
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("TEST!", "TEST2");
+        testObject.saveInBackground();
+
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
